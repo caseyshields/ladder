6d9f9161da341a8126a9f412590369d2fda463b1
caseyshields <caseyorigami@gmail.com>
2018-03-08 15:56:21 -0800

    Got test page to show styled sources

--- src/ladder.js
+++ src/ladder.js
@@ -1,19 +1,4 @@
 /* Factory function which returns a d3 component for drawing ladder diagrams */
-/* the ladder svg will look something like;
-<g class="ladder">
-<g id="timeAxis"></g>
-    <g id="legs">
-        <g class="...some ip">
-            <rect />
-            <text />
-        </g>
-        ...
-    </g>
-    <g id="rungs">
-        <path class="...some protocol">
-        ...
-    </g>
-</g>*/
 function createLadder( svg, id, left, top, width, height ) {
     
     let time, interval;
@@ -21,20 +6,18 @@ function createLadder( svg, id, left, top, width, height ) {
     let events = []; // the rungs of the ladder
 
     // set up the structure of the ladder svg
-    let ladder = svg.append('g')
+    let group = svg.append('g')
         .attr( 'id', id )
         .attr( 'class', 'ladder' );
 
-    let axis = ladder.append( 'g' )
-        .attr( 'g', 'axis' );
+    let axis = group.append( 'g' )
+        .attr( 'id', 'axis' );
     
-    let legs = ladder
-        .append( 'g' )
+    let legs = group.append( 'g' )
         .attr( 'id', 'legs' )
         .selectAll('g');
     
-    let rungs = ladder
-        .append( 'g' )
+    let rungs = group.append( 'g' )
         .attr( 'id', 'rungs' )
         .selectAll('g');
 
@@ -54,10 +37,15 @@ function createLadder( svg, id, left, top, width, height ) {
     //     .tickSize( width );
     // // we may want to customize the axis by selecting and modifying the axis after its creation- though this strikes me as more inefficient...
 
-    let ladder = function() {
-        //todo 
-        // draw the ladder
+    let ladder = function() { //todo make this accept a selection? 
+        
+        // join with the source data set
         legs = legs.data( sources );
+
+        // remove old sources
+        legs.exit().remove();
+
+        // create new timelines for each source
         let entered = legs.enter()
             .append( 'g' )
             .attr('class', function(d) {return d.class;} );
@@ -69,33 +57,49 @@ function createLadder( svg, id, left, top, width, height ) {
             .attr( 'height', sourceScale.bandwidth() );
 
         entered.append( 'text' )
-            .text( function(d) {return d.id;} );
+            .text( function(d) {return d.id;} )
+            .attr( 'anchor', 'start' )
+            .attr( 'x', left )
+            .attr( 'y', function(d) {return sourceScale(d.id);} );
         
+        // update all sources
         legs = entered.merge( legs );
             // .call( d3.drag()
             //     .on('start', started)
             //     .on('drag', dragged)
             //     .on('end', ended)
-            // ); // TODO filter or highlight on source selection?
-
-        selection.exit().remove();
-
-        selection.enter()
-        .append
-        
+            // ); // TODO filter or highlight on source selection? Allow re-ordering?
     }
 
-    ladder.sources = function( sources ) {
-        if (!sources) return sources;
-        ladder.sources = sources;
+    ladder.sources = function( newSources ) {
+        if (!newSources) return sources;
+        sources = newSources;
+        let ids = [];
+        newSources.forEach( function(d){ids.push(d.id);} );
+        sourceScale.domain( ids );
         return ladder;
     }
 
-    ladder.events = function( events ) {
-        if (!events) return events;
-        ladder.events = events;
+    ladder.events = function( newEvents ) {
+        if (!newEvents) return events;
+        events = newEvents;
         return ladder;
     }
 
     return ladder;
 }
+/* the ladder svg will look something like;
+<g class="ladder">
+<g id="timeAxis"></g>
+    <g id="legs">
+        <g class="...some ip">
+            <rect />
+            <text />
+        </g>
+        ...
+    </g>
+    <g id="rungs">
+        <path class="...some protocol">
+        ...
+    </g>
+</g>*/
